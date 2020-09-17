ba51c30b2045730502ccaf7e18571ad65c2bb69e
Hugo <>
2020-09-17 16:02:08 -0400

    initial setup 1. Spring Web 2. Spring Data Mongo 3. Gradle and war

---
+++ src/main/java/com/hugoqrbook/hugoqrbookservice/HugoqrbookserviceApplication.java
@@ -0,0 +1,13 @@
+package com.hugoqrbook.hugoqrbookservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HugoqrbookserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HugoqrbookserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hugoqrbook/hugoqrbookservice/HugoqrbookserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugoqrbook.hugoqrbookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HugoqrbookserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
