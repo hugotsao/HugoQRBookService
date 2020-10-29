63b307db48164cce2a713292b348013c5ccedcb4
Hugo <>
2020-10-29 11:15:34 -0400

    use spring session

---
+++ src/main/java/com/hugo/hugoqrbookservice/controllers/UsersController.java
@@ -0,0 +1,23 @@
+package com.hugo.hugoqrbookservice.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import java.security.Principal;
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost"})
+public class UsersController {
+    @GetMapping("/api/user/get")
+    public Principal getUserPrincipal(Principal user) {
+        return user;
+    }
+    @GetMapping("/api/token/get")
+    public Map<String,String> token(HttpSession session) {
+        return Collections.singletonMap("token", session.getId());
+    }
+}
